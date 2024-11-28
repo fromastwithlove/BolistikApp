73d9dd2586a3b40dd5ac1151d415e92edc9d27c8
Adil Yergaliyev <adil.yergaliyev@gmail.com>
2024-11-28 20:04:17 +0100

    Initial commit.

---
+++ bolistik/bolistikTests/bolistikTests.swift
@@ -0,0 +1,17 @@
+//
+//  bolistikTests.swift
+//  bolistikTests
+//
+//  Created by Adil Yergaliyev on 28.11.24.
+//
+
+import Testing
+@testable import bolistik
+
+struct bolistikTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
